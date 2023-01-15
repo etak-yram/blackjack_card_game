07dcbae59bd414c5c209386ac5608501dd7fb8df
MK <etakyram.img@gmail.com>
2023-01-15 10:40:01 +0000

    Update with functions file: check for blackjack evaluate scores

---
+++ blackjack_functions.py
@@ -0,0 +1,28 @@
+
+
+# check for blackjack
+def blackjack_check(player_score, dealer_score):
+    if player_score == 21 or dealer_score == 21:
+        return True
+
+# evaluate scores
+def evaluate_final_scores(player_score, dealer_score):
+    if (player_score == 21) or (dealer_score == 21):
+        if player_score == 21:
+            print('Game ends - Player Has Blackjack!\nPlayer Wins!')
+        else:
+            print('Game ends - Dealer Has Blackjack!\nDealer Wins!')
+
+    elif (player_score > 21) or (dealer_score > 21):
+        if player_score > 21:
+            print('Game ends - Player Bust!\nDealer Wins!')
+        else:
+            print('Game ends - Dealer Bust!\nPlayer Wins!')
+
+    else:
+        if dealer_score < player_score < 21:
+            print('Player Wins!')
+        elif player_score < dealer_score < 21:
+            print('Dealer Wins!')
+        else:
+            print('It looks like a draw...')
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+from blackjack_functions import *
 from blackjack_classes import Deck, Hand
+
 
 
 def play():
@@ -13,6 +15,7 @@ def play():
         dealer_hand.add_card(deck.deal_card())
         dealer_hand.add_score()
 
+    # show 'dealers hand' - only first card
     dealer_hand.show_dealer_hand()
 
     # deal players hand
@@ -25,67 +28,67 @@ def play():
     print(player_hand.deck)
     player_hand.show_score()
 
+
+    # implement the game loop to run whilst player and dealer scores are under 21
+
     while dealer_hand.score < 21 and player_hand.score < 21:
 
+        # first check for blackjack, if so break
         if dealer_hand.score == 21 or player_hand.score == 21:
             break
 
+        # check dealer score and hit a card if it's under 17
         if dealer_hand.score < 17:
             print('\nDealer Hits!\n')
             dealer_hand.add_card(deck.deal_card())
             dealer_hand.add_score()
             dealer_hand.show_score()
+
+            # check if dealer is bust, if so break
             if dealer_hand.score > 21:
                 print('Dealer Bust!')
                 break
 
+        # implement a variable to prompt player for next move hit/stand
         next_move = input('Player: Hit or Stand?')
+
+        # if hit is selected add card to hand and update score, print out new hand
         if next_move == 'Hit':
             player_hand.add_card(deck.deal_card())
             player_hand.add_score()
             print(player_hand.deck)
             player_hand.show_score()
+
+            # check if player is bust, if so, break
             if player_hand.score > 21:
                 print('Player Bust!')
                 break
+
+        # if stand is selected, break out the while loop
         elif next_move == 'Stand':
             print('Player Stands!')
             break
+
+        # if no break conditions satisfied, loop around again
         continue
 
+
+    # if loop is broken, message print to adv scores are being checked
     print('\nChecking Scores...\n')
 
-    if (player_hand.score == 21) or (dealer_hand.score == 21):
-        if player_hand.score == 21:
-            print('Game ends - Player Has Blackjack!')
-            player_hand.show_score()
-            dealer_hand.show_score()
-        else:
-            print('Game ends - Dealer Has Blackjack!')
-            player_hand.show_score()
-            dealer_hand.show_score()
-    elif (player_hand.score > 21) or (dealer_hand.score > 21):
-        if player_hand.score > 21:
-            print('Game ends - Player Bust!')
-            player_hand.show_score()
-            dealer_hand.show_score()
-        else:
-            print('Game ends - Dealer Bust!')
-            player_hand.show_score()
-            dealer_hand.show_score()
+    # check final scores
+    evaluate_final_scores(player_hand.score, dealer_hand.score)
+
+    # show final scores
+    player_hand.show_score()
+    dealer_hand.show_score()
+
+
+    play_again = input('\nWould you like to play again? Y/N: ')
+    if play_again == 'Y':
+        play()
     else:
-        if dealer_hand.score < player_hand.score < 21:
-            print('Player Wins!')
-            player_hand.show_score()
-            dealer_hand.show_score()
-        elif player_hand.score < dealer_hand.score < 21:
-            print('Dealer Wins!')
-            dealer_hand.show_score()
-            player_hand.show_score()
-        else:
-            print('It looks like a draw...')
-            dealer_hand.show_score()
-            player_hand.show_score()
+        print('\nThanks for playing!')
 
 
 if __name__ == '__main__':
